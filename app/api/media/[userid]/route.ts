44493ca7f077806a31d0aec351ca89387fad22fc
deeeu <yogalmo100@gmail.com>
2024-10-11 16:09:21 -0500

    apis working to fetch data

--- app/api/media/[userid]/route.ts
+++ app/api/media/[userid]/route.ts
@@ -1,12 +1,21 @@
 import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
-
+import { SmallMedia } from '@/types/media'
 const prisma = new PrismaClient();
 
 export async function GET(request: Request, { params }: { params: { userid: string } }) {
+  
+  if (!params.userid) {
+    return NextResponse.json({ error: 'User ID is required' }, { status: 400 });
+  } 
+  
   try {
-    const media = await prisma.media.findMany({
-      where: { user_id: params.userid }
+    const media: SmallMedia[] = await prisma.media.findMany({
+      where: { user_id: params.userid },
+      select: {
+        id: true,
+        name: true,
+      },
     });
 
     return NextResponse.json(media);
